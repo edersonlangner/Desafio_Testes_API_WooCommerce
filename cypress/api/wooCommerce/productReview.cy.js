dd96217a5bf046ebb3ff1747cd3f7e100234e694
Ederson Langner <edersonlangner@gmail.com>
2022-11-16 17:02:13 -0300

    Desafio Testes de API v2 + Faker

--- cypress/api/wooCommerce/productReview.cy.js
+++ cypress/api/wooCommerce/productReview.cy.js
@@ -8,12 +8,14 @@ import productReviewWooCommerceSchema from '../../contracts/productReview.contra
 describe('Product Review WooCommerce', () => {
     it('Criar Product Review', () => {
         const review = faker.company.name()
+        const reviewer = faker.name.firstName()
+        const reviewerEmail = faker.internet.email(reviewer)
         cy.postProductReviewWooCommerce(
             tokenFixture.token,
             productsFixture.ProductsValido.product_id,
             review,
-            productsFixture.ProductsValido.reviewer,
-            productsFixture.ProductsValido.reviewer_email,
+            reviewer,
+            reviewerEmail,
             productsFixture.ProductsValido.rating,
         )
             .then((response) => {
@@ -21,8 +23,8 @@ describe('Product Review WooCommerce', () => {
                 expect(response.status).to.eq(statusFixtures.Created)
                 expect(response.body.product_id).eq(productsFixture.ProductsValido.product_id)
                 expect(response.body.review).eq(review)
-                expect(response.body.reviewer).eq(productsFixture.ProductsValido.reviewer)
-                expect(response.body.reviewer_email).eq(productsFixture.ProductsValido.reviewer_email)
+                expect(response.body.reviewer).eq(reviewer)
+                expect(response.body.reviewer_email).eq(reviewerEmail)
                 expect(response.body.rating).eq(productsFixture.ProductsValido.rating)
                 return productReviewWooCommerceSchema.validateAsync(response.body),
                 cy.deleteProductReviewWooCommerce(
@@ -45,13 +47,15 @@ describe('Product Review WooCommerce', () => {
         )
             .then((response) => {
                 const reviewEditar = faker.animal.cat()
+                const reviewer = faker.name.firstName()
+                const reviewerEmail = faker.internet.email(reviewer)
                 var id = response.body.id
                 cy.putProductReviewWooCommerce(
                     tokenFixture.token,
                     productsFixture.ProductsEditar.product_id,
                     reviewEditar,
-                    productsFixture.ProductsEditar.reviewer,
-                    productsFixture.ProductsEditar.reviewer_email,
+                    reviewer,
+                    reviewerEmail,
                     productsFixture.ProductsEditar.rating,
                     id
 
@@ -60,8 +64,8 @@ describe('Product Review WooCommerce', () => {
                     expect(response.status).to.eq(statusFixtures.ok)
                     expect(response.body.product_id).to.eq(productsFixture.ProductsEditar.product_id)
                     expect(response.body.review).to.eq(reviewEditar)
-                    expect(response.body.reviewer).to.eq(productsFixture.ProductsEditar.reviewer)
-                    expect(response.body.reviewer_email).to.eq(productsFixture.ProductsEditar.reviewer_email)
+                    expect(response.body.reviewer).to.eq(reviewer)
+                    expect(response.body.reviewer_email).to.eq(reviewerEmail)
                     expect(response.body.rating).to.eq(productsFixture.ProductsEditar.rating)
                     return productReviewWooCommerceSchema.validateAsync(response.body),
                     cy.deleteProductReviewWooCommerce(
@@ -78,12 +82,14 @@ describe('Product Review WooCommerce', () => {
     })
     it('Deletar Product Review', () => {
         const review = faker.name.firstName()
+        const reviewer = faker.name.firstName()
+        const reviewerEmail = faker.internet.email(reviewer)
         cy.postProductReviewWooCommerce(
             tokenFixture.token,
             productsFixture.ProductsValido.product_id,
             review,
-            productsFixture.ProductsValido.reviewer,
-            productsFixture.ProductsValido.reviewer_email,
+            reviewer,
+            reviewerEmail,
             productsFixture.ProductsValido.rating,
         )
             .then((response) => {
@@ -97,8 +103,8 @@ describe('Product Review WooCommerce', () => {
                     expect(response.status).to.eq(statusFixtures.ok)
                     expect(response.body.previous.product_id).eq(productsFixture.ProductsValido.product_id)
                     expect(response.body.previous.review).eq(review)
-                    expect(response.body.previous.reviewer).eq(productsFixture.ProductsValido.reviewer)
-                    expect(response.body.previous.reviewer_email).eq(productsFixture.ProductsValido.reviewer_email)
+                    expect(response.body.previous.reviewer).eq(reviewer)
+                    expect(response.body.previous.reviewer_email).eq(reviewerEmail)
                     expect(response.body.previous.rating).eq(productsFixture.ProductsValido.rating)
                     return productReviewWooCommerceSchema.validateAsync(response.body.previous)
                 })
